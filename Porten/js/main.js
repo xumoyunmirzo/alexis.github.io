cdfef8ce22d0bb6245cfc37cef459702a5ec6dcc
xumoyunmirzo <khumoyun.khushvakov@gmail.com>
2021-01-05 12:38:07 +0500

    Переделали сайт Porten

---
+++ Porten/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+	function ibg(){
+		$.each($('.ibg'), function(index, val) {
+			if($(this).find('img').length>0){
+				$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+			}
+		});
+	}
+	ibg();
+});
